ee99eb2046dbdddc2eaf0b24498112dc6e225d97
ArthurAk <amirmahdiakhi@gmail.com>
2023-11-28 09:18:47 +0330

    Add Groups To UsersAdmin

--- usersadmin/urls.py
+++ usersadmin/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.stadio, name='stadio')
+    path('', views.stadio, name='stadio'),
+    path('users/', views.index_users, name='stadio.index_users'),
+    path('groups/', views.index_groups, name='stadio.index_groups'),
 ]
--- usersadmin/views.py
+++ usersadmin/views.py
@@ -1,5 +1,8 @@
+from django.contrib import messages
+from django.contrib.auth import get_user_model
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.contrib.auth.models import Group
+from django.shortcuts import render, redirect
 
 from videos.models import Video
 
@@ -17,3 +20,31 @@ def stadio(request):
     }
 
     return render(request, "usersadmin/index.html", context)
+
+
+@login_required()
+def index_users(request):
+
+    users = get_user_model().objects.all
+    return render(request, 'usersadmin/index_users.html', {'users': users})
+
+
+def index_groups(request):
+
+    groups = Group.objects.all
+    return render(request, 'usersadmin/index_groups.html', {'groups': groups})
+
+
+def create_group(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        group_exists = Group.objects.filter(name=name)
+        if group_exists:
+            messages.warning(request, "group already exists")
+            return render(request, "usersadmin/create_group.html", {"message": messages})
+        group = Group.objects.create(name=name)
+        return redirect('usersadmin/index')
+    else:
+        return render(request, 'usersadmin/create_group.html')
+
+
