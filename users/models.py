952f715d4579831780f81aa7cabff734a2121213
ArthurAk <amirmahdiakhi@gmail.com>
2023-11-29 02:55:39 +0330

    Create Custom Group And Permission Model

--- users/models.py
+++ users/models.py
@@ -24,6 +24,27 @@ class Profile(models.Model):
         return self.user.username
 
 
+class Permission(models.Model):
+    codename = models.CharField(max_length=100)
+    name = models.CharField(max_length=200, null=True)
+    description = models.TextField(null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Group(models.Model):
+    name = models.CharField(max_length=100, null=True)
+    description = models.TextField(null=True)
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    permissions = models.ManyToManyField(Permission, related_name='groups')
+
+    def __str__(self):
+        return self.name
+
+
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
